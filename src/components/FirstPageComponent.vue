5cd2b372ffafeeb74af031483773a406efab9029
ahamad236 <ahmadshaik4u@gmail.com>
2024-07-17 14:29:38 +0530

    first commit

---
+++ src/components/FirstPageComponent.vue
@@ -0,0 +1,42 @@
+<!--  old approach with defineProps and defineEmits -->
+<!-- <template>
+    <h2>
+        First Page Component
+    </h2>
+    <button @click="update">Update</button>
+    <div>
+        Salary in Child Component is : {{ salary }}
+    </div>
+</template>
+
+<script setup lang="ts">
+    defineProps(['salary'])
+
+    const emits = defineEmits(['update'])
+
+    function update(){
+        emits('update')
+    }
+</script> -->
+<!-- old approach upto above -->
+
+
+<!-- new approad using component v-model from below -->
+<template>
+    <h2>
+        First Page Component
+    </h2>
+    <button @click="update">Update</button>
+    <div>
+        Salary in Child is : {{ model }}
+    </div>
+</template>
+
+<script setup lang="ts">
+    const model:any = defineModel()
+
+    function update(){
+        model.value++
+    }
+</script>
+<!-- new approach upto above -->
---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,42 @@
+<!-- old approach with defineProps and defineEmits -->
+<!-- <template>
+    <h1>
+        Home Component
+    </h1>
+    <div>
+        Salary in Parent Component is : {{ Salary }}
+    </div>
+    <FirstPage :salary="Salary" @update="update"></FirstPage>
+</template>
+
+<script setup lang="ts">
+    import FirstPage from './FirstPageComponent.vue'
+    import { ref } from 'vue'
+
+    const Salary = ref(28000)
+
+    function update(){
+        Salary.value++
+    }
+</script> -->
+<!-- old approach upto above -->
+
+
+<!-- new approad using component v-model from below -->
+<template>
+    <h1>
+        Home Component
+    </h1>
+    <div>
+        Salary in Home is : {{ salary }}
+    </div>
+    <FirstPage v-model="salary"></FirstPage>
+</template>
+
+<script setup lang="ts">
+    import FirstPage from "./FirstPageComponent.vue"
+    import { ref } from "vue"
+
+    const salary = ref<number>(28000)
+</script>
+<!-- new approach upto above -->
